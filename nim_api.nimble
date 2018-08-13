6e1ec9473305152112094df482dfb0d3f817056a
Toshiki Ohnogi <thetoshiki0419@gmail.com>
2018-08-13 14:50:02 +0900

    initial: テーブル参照まで

---
+++ nim_api.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Toshiki Ohnogi"
+description   = "A new awesome nimble package"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["nim_api"]
+
+# Dependencies
+
+requires "nim >= 0.18.0"
---
+++ src/nim_api.nim
@@ -0,0 +1,54 @@
+import jester, asyncdispatch, json, db_mysql
+
+
+# set db
+let db = open("192.168.1.15", "root", "hogehoge", "nim_api")
+
+# router
+routes:
+
+  # root
+  get "/":
+    resp "Hello World!"
+
+  # information of all users
+  get "/users":
+
+    # query
+    var rows = db.getAllRows(sql"select * from memberlist")
+
+    # format row datas
+    var users: seq[JsonNode]
+    users = @[]
+    for row in rows:
+      users.add(%*{
+        "id": row[0],
+        "name": row[1],
+        "place": row[2],
+        "age": row[3],
+      })
+    
+    # set to data
+    var data = %*{
+      "total": users.len,
+      "users": users,
+    }
+
+    resp $data, "application/json"
+
+  # information of a user
+  get "/users/@username":
+
+    # query
+    var row = db.getRow(sql"select * from memberlist where name = ?", @"username")
+
+    # set to object
+    var data = %*{
+      "id": row[0],
+      "name": row[1],
+      "place": row[2],
+      "age": row[3],
+    }
+    resp $data, "application/json"
+
+runForever()
